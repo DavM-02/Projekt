966a3d9476544d847c0b9dafec468cd10ae4e9af
LukaszMajchrzak3 <133999034+LukaszMajchrzak3@users.noreply.github.com>
2023-06-04 17:10:27 +0200

    Delete PlatformObject.h

--- PlatformObject.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "GameObject.h"
-class PlatformObject : public GameObject
-{
-public:
-	PlatformObject(sf::Vector2f _size) :GameObject(_size) {}
-	void animate(const float& elapsed) {}
-};
-
