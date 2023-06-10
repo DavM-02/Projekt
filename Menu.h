8da67c853d76c02b22ef97fd39a55331dab84cba
LukaszMajchrzak3 <133999034+LukaszMajchrzak3@users.noreply.github.com>
2023-06-10 19:09:11 +0200

    Add files via upload

---
+++ Menu.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
+
+class Menu
+{
+    sf::Texture* main_texture;
+    sf::Sprite* _texture;
+    std::vector<sf::Texture*>_other_textures;
+    std::vector<sf::RectangleShape>positoned_textures;
+public:
+    Menu(std::string _texture_file);
+    sf::Sprite* get_main_texture();
+    void set_textures();
+    std::vector<sf::RectangleShape> get_positoned_textures();
+    std::vector<sf::Vector2f> get_buttons_positions();
+};
